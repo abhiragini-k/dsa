0919f845aa3666da9d75b563a2c7fedda524a2b9
Abhiragini K <24ug1bycs684@bmsit.in>
2026-06-27 17:24:59 +0530

    Time: 3 ms (29.05%), Space: 10.1 MB (38.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,19 +4,10 @@ public:
         int start=0;
         int end=s.size()-1;
         while(start<=end){
-            if(!isalnum(s[start])){
-                start++;
-                continue;
-            }
-            if(!isalnum(s[end])){
-                end--;
-                continue;
-            }
-            if(tolower(s[start])!=tolower(s[end])) return false;
-            else{
-                start++;
-                end--;
-            }
+            if(!isalnum(s[start])) start++;
+            else if(!isalnum(s[end])) end--;
+            else if(tolower(s[start])!=tolower(s[end])) return false;
+            else {start++;end--;}
         }
         return true;
     }

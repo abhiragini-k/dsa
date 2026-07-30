1e7750ddcae46027bc61e2bbbc0d07ecf84abc84
Abhiragini K <24ug1bycs684@bmsit.in>
2026-07-30 10:02:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int count=0;
+        int n=word.length();
+        /*
+        8 number in keypads
+        g1:0-7 count=1 , g2:8-15 count=2 , so on...
+        so if n/8 we will get no of filled groups
+        the remaining no will be stored in rem ... this was obviously is filling the next group which is partial 
 
-        for(int i=0;i<word.length();i++){
-            count+=i/8 + 1;
-        }
-        return count;
+        so...
+
+        for filled group:
+            =8*1+8*2+....+8*group
+            =8(1+2+...+group)
+            =8(group(group+1)/2)
+            =4(group(group+1))
+
+        for partial group:
+            =remaining_numbers*(total_no_of_filled_groups+1)
+            =rem*(group+1)
+
+        hence final formula:
+            = 4(group(group+1)) + rem*(group+1)
+        */
+        int group=n/8;
+        int rem=n%8;
+        return 4*group*(group+1) + rem*(group+1);
     }
 };

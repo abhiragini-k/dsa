21f49a22a94210432e76769ab3c987ba4cc2a9bd
Abhiragini K <24ug1bycs684@bmsit.in>
2026-07-30 09:47:55 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int count=0;
+
         for(int i=0;i<word.length();i++){
-            if(i<8){
-                count+=1;
-            }else if(i>=8 && i<16){
-                count+=2;
-            }else if(i>=16 && i<24){
-                count+=3;
-            }else{
-                count+=4;
-            }
+            count+=i/8 + 1;
         }
         return count;
     }

1243d9f91d23792070564bd580b6eb14e5a8c9f2
Abhiragini K <24ug1bycs684@bmsit.in>
2026-09-11 20:25:32 +0530

    Time: 1 ms (57.14%), Space: 8.5 MB (64.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0 || (x%10==0 && x!=0)) return false;
+        int temp=x;
         int rev=0;
-        while(x>rev){
-            rev = rev*10 + x%10;
+        if(x<0) return false;
+        while(x>0){
+            int digit=x%10;
+            if(rev>INT_MAX/10 ||rev<INT_MIN/10) return false;
+            rev=rev*10+digit;
             x=x/10;
         }
-        return (x==rev || x==rev/10);
+        if(rev==temp) return true;
+        else return false;
     }
 };
